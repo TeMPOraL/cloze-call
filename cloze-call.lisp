d0433aa81cd3eddeaef17ebf376ed3433ee358f5
temporal.pl <temporal.pl@6847b43b-0fb9-b1c9-0603-a5bce3d80f79>
2010-03-27 15:12:40 +0000

    Minor tweaks + new game state - intro screen.
    New issue
    Summary: Screen Game State doesn't have a working update function.
    Labels: Type-Defect Component-Logic Priority-Critical
    We need to add timeout and keyboard skipping features to game screens.
    
    New Issue
    Summary: SDL_image doesn't work
    Labels: Type-Enhancement
    Making SDL_image work would allow us to use PNG and JPG
    for graphics, and thus use less disk space to keep images.

--- cloze-call.lisp
+++ cloze-call.lisp
@@ -6,6 +6,7 @@
 (load "math.lisp")      ; math module
 (load "game-state-manager.lisp") ; game state manager module
 (load "test-game-state.lisp") ; test game state
+(load "game-screen-state.lisp") ; game screens - victory, intro, ect.
 (load "game-objects.lisp") ; game classes
                            ; FIXME move to game state :)
 
@@ -21,13 +22,15 @@
     (with-game-state-manager gsm
       ;;; REGISTER GAME STATES
       (register-game-state gsm :test-state (make-instance 'test-game-state))
-      (change-state gsm :test-state)
+      (register-game-state gsm :intro-screen (make-instance 'screen-game-state
+                                                            :picture-name "intro.bmp"
+                                                            :next-state :test-state))
+      (change-state gsm :intro-screen)
       ;;; EVENT PUMP / MAIN LOOP
       (sdl:with-events ()
 
         ;; UPDATE EVENT
         (:idle
-         (sdl:clear-display sdl:*green*)
          (sdl:with-timestep ()
            ;; update game state manager here
            (update-gsm gsm (sdl:dt)))
